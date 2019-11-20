8fbbd4e1b552f200a708e80bbff114f853031ca1
NikandrovS <39054635+NikandrovS@users.noreply.github.com>
2019-11-21 02:26:28 +0300

    create new events
    Now you can easily create new events from /main page

--- models/query.js
+++ models/query.js
@@ -1,25 +1,34 @@
 const query = require('mysql-query-promise');
 const config = require('config');
+const tableName = config.product.mainTable;
+
 
 const crud = {
     getAll: async () => {
-        return query('SELECT id, event, until, result1, SUM(bet1) as total1, result2, SUM(bet2) as total2,status\n' +
-            'FROM events, deposits WHERE events.id=deposits.event_id\n' +
-            'GROUP BY id\n' +
-            'ORDER BY status;');
-        // return query(`SELECT * FROM events ORDER BY status;`);
+        return query(`SELECT id, event, until, result1, SUM(bet1) as total1, result2, SUM(bet2) as total2,status
+            FROM events, deposits WHERE events.id=deposits.event_id
+            GROUP BY id
+            ORDER BY status
+            LIMIT 20 OFFSET 0;`);
     },
     getById: async (id) => {
         return query(`SELECT id, event, until, result1, SUM(bet1) as total1, result2, SUM(bet2) as total2,status
-            FROM events, deposits WHERE events.id=deposits.event_id AND id='?'
+            FROM events, deposits WHERE events.id=deposits.event_id AND id = ?
             GROUP BY id
-            ORDER BY status;`,[Number(id)]);
+            ORDER BY status;`, [Number(id)]);
     },
     getBets: async (id) => {
         return query(`SELECT bet1 + bet2 as bet, player FROM deposits
-            where event_id='?'
-            order by bet desc
+            WHERE event_id = ?
+            ORDER BY bet desc
             LIMIT 5 OFFSET 0;`, [Number(id)]);
-    }
+    },
+    createEvent: async ( {title, until, result1, result2} ) => {
+        return query(`INSERT INTO ${tableName} (event, until, result1, result2) 
+            VALUES (?, ?, ?, ?);`,  [title, until, result1, result2]);
+    },
+    getLastId: async () => {
+        return query(`SELECT max(id) AS id FROM ${tableName}`);
+    },
 };
 module.exports = crud;
